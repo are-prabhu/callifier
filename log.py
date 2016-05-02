007038afa74d439f053f265bb957c09a8b385879
Prabhu Are <flyingcolours47@gmail.com>
2016-05-02 13:00:57 +0530

    remove log.py

--- log.py
+++
@@ -1,9 +0,0 @@
-from utils.log import Logger
-logging = Logger.getLogger(__file__)
-
-
-logging.debug("Exception for request Environ")
-logging.info("Exception for request Environ")
-logging.warning("Exception for request Environ")
-logging.error("Exception for request Environ")
-logging.critical("Exception for request Environ")
